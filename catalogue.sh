d7010daefd0ebdc1c08ddd7d339e36a95b74399f
nagendra <nagenderababu.g@gmail.com>
2023-12-16 15:30:12 +0530

    all scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,39 @@
+echo -e "\e[36m>>>>>>>>> Configuring NodeJs repos <<<<<<<<<<<\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+
+echo -e "\e[36m>>>>>>>>> Install NodeJs <<<<<<<<<<<\e[0m"
 dnf install nodejs -y
+
+echo -e "\e[36m>>>>>>>>> Add Application user <<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<<<\e[0m"
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+
+echo -e "\e[36m>>>>>>>>> Install NodeJs Dependencies <<<<<<<<<<<\e[0m"
 npm install
+
+echo -e "\e[36m>>>>>>>>> Copy Catalogue Systemd file <<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>>>>> Start Catalogue Service <<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 
+echo -e "\e[36m>>>>>>>>> Copy Mongo repo <<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y
+
+echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<<\e[0m"
 mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js

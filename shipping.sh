b2ac76782aa1ec0bdbbc2d107756fc03ec3523d9
nagendra <nagenderababu.g@gmail.com>
2023-12-17 20:58:46 +0530

    all scripts

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,34 @@
+echo -e "\e[36m>>>>>>>>> Install Maven <<<<<<<<<<<\e[0m"
 dnf install maven -y
+
+echo -e "\e[36m>>>>>>>>> Create App User <<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>> Create App Directory <<<<<<<<<<<\e[0m"
+rm-rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+echo -e "\e[36m>>>>>>>>> Extract App Content <<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>> Download Maven Dependencies <<<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-cp shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[36m>>>>>>>>> Install NodeJs <<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[36m>>>>>>>>> Install Mysql <<<<<<<<<<<\e[0m"
+dnf install mysql -y
+
+echo -e "\e[36m>>>>>>>>> Install Mysql <<<<<<<<<<<\e[0m"
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>>>>>> Install NodeJs <<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
-systemctl start shipping
-dnf install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping

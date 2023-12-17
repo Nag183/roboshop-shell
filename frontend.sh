573fff87c6f12338e4422d2d7eeecc4b7cabde67
nagendra <nagenderababu.g@gmail.com>
2023-12-17 21:19:59 +0530

    all scripts

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,20 @@
+echo -e "\e[36m>>>>>>>>> Install Nginx <<<<<<<<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[36m>>>>>>>>> Copy roboshop Config file <<<<<<<<<<<\e[0m"
 cp roboshop.conf /etc/ngnix/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>>> Clean Old App content <<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36m>>>>>>>>> Extracting App Content <<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m>>>>>>>>> Start Nginx <<<<<<<<<<<\e[0m"
 systemctl restart nginx
 systemctl enable nginx
 

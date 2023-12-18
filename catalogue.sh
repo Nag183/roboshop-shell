9837ce62ef87c3e4fcb47ea85708e793be3c4b01
nagendra <nagenderababu.g@gmail.com>
2023-12-18 14:09:44 +0530

    all scripts

--- catalogue.sh
+++ catalogue.sh
@@ -6,11 +6,4 @@ component=catalogue
 
 func_nodejs
 
-echo -e "\e[36m>>>>>>>>> Copy Mongo repo <<<<<<<<<<<\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y
-
-echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<<\e[0m"
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
--- common.sh
+++ common.sh
@@ -2,35 +2,52 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
+print_head() {
+  echo -e "\e[36m>>>>>>>>> $* <<<<<<<<<<<\e[0m"
+}
+
+schema_setup() {
+  echo -e "\e[36m>>>>>>>>> Copy Mongo repo <<<<<<<<<<<\e[0m"
+  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+
+  echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<<<<\e[0m"
+  dnf install mongodb-org-shell -y
+
+  echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<<\e[0m"
+  mongo --host MONGODB-SERVER-IPADDRESS </app/schema/${component}.js
+}
+
 func_nodejs() {
-  echo -e "\e[36m>>>>>>>>> Disable Nodejs  <<<<<<<<<<<\e[0m"
+  print_head "Disable Nodejs"
   dnf module disable nodejs -y
   dnf module enable nodejs:18 -y
 
-  echo -e "\e[36m>>>>>>>>> Install NodeJs <<<<<<<<<<<\e[0m"
+  print_head "Install NodeJs "
   dnf install nodejs -y
 
-  echo -e "\e[36m>>>>>>>>> Add Application cart <<<<<<<<<<<\e[0m"
+  print_head "Add Application cart"
   useradd ${app_user}
 
-  echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<<<\e[0m"
+  print_head "Create Application Directory"
   mkdir /app
 
-  echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<<<<\e[0m"
+  print_head "Download App Content"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
-  echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<<<<\e[0m"
+  print_head "Unzip App Content"
   unzip /tmp/${component}.zip
 
-  echo -e "\e[36m>>>>>>>>> Install NodeJs Dependencies <<<<<<<<<<<\e[0m"
+  print_head "Install NodeJs Dependencies"
   npm install
 
-  echo -e "\e[36m>>>>>>>>> Copy cart Systemd file <<<<<<<<<<<\e[0m"
+  print_head "Copy cart Systemd file"
   cp $script_path/${component}.service /etc/systemd/system/${component}.service
 
-  echo -e "\e[36m>>>>>>>>> Start cart Service <<<<<<<<<<<\e[0m"
+  print_head "Start cart Service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
+
+  schema_setup
 }

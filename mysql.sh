fc7bfb9ccc2a6d0ded43f37f8c31a2ffe8fa779d
nagendra <nagenderababu.g@gmail.com>
2023-12-18 14:32:22 +0530

    all scripts

--- mysql.sh
+++ mysql.sh
@@ -2,6 +2,13 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+mysql_root_password=$1
+
+if [ -z "$mysql_root_password" ]; then
+  echo Input Mysql Root Password Missing
+  exit
+fi
+
 echo -e "\e[36m>>>>>>>>> Disable Mysql <<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
 
@@ -16,4 +23,4 @@ systemctl enable mysqld
 systemctl start mysqld
 
 echo -e "\e[36m>>>>>>>>> Reset Mysql Password <<<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass $mysql_root_password

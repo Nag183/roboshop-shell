d378b8c90e2a82e391778e208d8bb067c0d6a4ad
nagendra <nagenderababu.g@gmail.com>
2023-12-22 11:44:06 +0530

    all scripts

--- mysql.sh
+++ mysql.sh
@@ -1,27 +1,27 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-
 mysql_root_password=$1
 
 if [ -z "$mysql_root_password" ]; then
-  echo Input Mysql Root Password Missing
+  echo Input MySQL Root Password Missing
   exit 1
 fi
+
 
 func_print_head "Disable MySQL 8 Version"
 dnf module disable mysql -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Copy MySQL Repo File"
-cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 func_stat_check $?
 
 func_print_head "Install MySQL"
-dnf install mysql-community-server -y &>>$log_file
+yum install mysql-community-server -y &>>$log_file
 func_stat_check $?
 
-func_print_head "Start MySQ"
+func_print_head "Start MySQL"
 systemctl enable mysqld &>>$log_file
 systemctl restart mysqld &>>$log_file
 func_stat_check $?

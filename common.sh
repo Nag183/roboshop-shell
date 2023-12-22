64e15a078ed6e7f9bfafd66e3b554fec8d92a28b
nagendra <nagenderababu.g@gmail.com>
2023-12-22 11:41:36 +0530

    all scripts

--- common.sh
+++ common.sh
@@ -4,9 +4,10 @@ script_path=$(dirname "$script")
 log_file=/tmp/roboshop.log
 # rm -f $log_file
 
+
 func_print_head() {
-  echo -e "\e[36m>>>>>>>>> $1 <<<<<<<<<<<\e[0m"
-  echo -e "\e[36m>>>>>>>>> $1 <<<<<<<<<<<\e[0m" &>>$log_file
+  echo -e "\e[35m>>>>>>>>> $1 <<<<<<<<\e[0m"
+  echo -e "\e[35m>>>>>>>>> $1 <<<<<<<<\e[0m" &>>$log_file
 }
 
 func_stat_check() {
@@ -21,31 +22,31 @@ func_stat_check() {
 
 func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
-    func_print_head "Copy Mongo repo"
+    func_print_head "Copy MongoDB repo"
     cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
     func_stat_check $?
 
     func_print_head "Install MongoDB Client"
-    dnf install mongodb-org-shell -y &>>$log_file
+    yum install mongodb-org-shell -y &>>$log_file
     func_stat_check $?
 
-    func_print_head "Load Schema "
-    mongo --host MONGODB-SERVER-IPADDRESS </app/schema/${component}.js &>>$log_file
+    func_print_head "Load Schema"
+    mongo --host mongodb-dev.rdevopsb72.online </app/schema/${component}.js &>>$log_file
     func_stat_check $?
   fi
   if [ "${schema_setup}" == "mysql" ]; then
-    func_print_head "Install Mysql"
-    dnf install mysql -y &>>$log_file
+    func_print_head "Install MySQL Client"
+    yum install mysql -y &>>$log_file
     func_stat_check $?
 
-    func_print_head "Install Mysql"
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file
+    func_print_head "Load Schema"
+    mysql -h mysql-dev.rdevopsb72.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file
     func_stat_check $?
   fi
 }
 
 func_app_prereq() {
-  func_print_head "Add Application User"
+  func_print_head "Create Application User"
   id ${app_user} &>>/tmp/roboshop.log
   if [ $? -ne 0 ]; then
     useradd ${app_user} &>>/tmp/roboshop.log
@@ -53,12 +54,12 @@ func_app_prereq() {
   func_stat_check $?
 
   func_print_head "Create Application Directory"
-  rm-rf /app &>>$log_file
+  rm -rf /app &>>$log_file
   mkdir /app &>>$log_file
   func_stat_check $?
 
   func_print_head "Download Application Content"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
   func_stat_check $?
 
   func_print_head "Extract Application Content"
@@ -68,11 +69,11 @@ func_app_prereq() {
 }
 
 func_systemd_setup() {
-  func_print_head "Copy User Systemd file"
-  cp $script_path/${component}.service /etc/systemd/system/${component}.service
+  func_print_head "Setup SystemD Service"
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
   func_stat_check $?
 
-  func_print_head "Start Shipping Service"
+  func_print_head "Start ${component} Service"
   systemctl daemon-reload &>>$log_file
   systemctl enable ${component} &>>$log_file
   systemctl restart ${component} &>>$log_file
@@ -80,18 +81,17 @@ func_systemd_setup() {
 }
 
 func_nodejs() {
-  func_print_head "Disable Nodejs"
-  dnf module disable nodejs -y &>>$log_file
-  dnf module enable nodejs:18 -y &>>$log_file
+  func_print_head "Configuring NodeJS repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
   func_stat_check $?
 
-  func_print_head "Install NodeJs "
-  dnf install nodejs -y &>>$log_file
+  func_print_head "Install NodeJS"
+  yum install nodejs -y &>>$log_file
   func_stat_check $?
 
   func_app_prereq
 
-  func_print_head "Install NodeJs Dependencies"
+  func_print_head "Install NodeJS Dependencies"
   npm install &>>$log_file
   func_stat_check $?
 
@@ -101,15 +101,15 @@ func_nodejs() {
 
 func_java() {
   func_print_head "Install Maven"
-  dnf install maven -y &>>$log_file
+  yum install maven -y &>>$log_file
   func_stat_check $?
 
   func_app_prereq
 
-   func_print_head "Download Maven Dependencies"
-   mvn clean package &>>$log_file
-   func_stat_check $?
-   mv target/${component}-1.0.jar ${component}.jar &>>$log_file
+  func_print_head "Download Maven Dependencies"
+  mvn clean package &>>$log_file
+  func_stat_check $?
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
 
   func_schema_setup
   func_systemd_setup

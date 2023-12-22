216b1a32eacac527701efc39fb455832b89f8539
nagendra <nagenderababu.g@gmail.com>
2023-12-22 11:42:50 +0530

    all scripts

--- mongodb.sh
+++ mongodb.sh
@@ -3,15 +3,15 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 func_print_head "Setup MongoDB Repo"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>>$log_file
 func_stat_check $?
 
-func_print_head "Install MongoDBo"
+func_print_head "Install MongoDB"
 yum install mongodb-org -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Update MongoDB Listen Address"
-sed -i -e 's|127.0.0.1|0.0.0.0' /etc/mongod.conf &>>$log_file
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
 func_stat_check $?
 
 func_print_head "Start MongoDB"
